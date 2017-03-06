0460e750155531e5a2fa7de9440193aa1fe51494
q-ode <bayodapo@gmail.com>
2017-03-06 16:15:23 +0100

    Log request

--- src/controllers/teamCtrl.js
+++ src/controllers/teamCtrl.js
@@ -1,7 +1,11 @@
+import log4js from 'log4js';
 import { teamFacade } from '../facades/teamFacade';
+
+const log = log4js.getLogger('teamFacade');
 
 const teamsCtrl = {
   create(req, res) {
+    log.info(req.body);
     const team = req.body.team;
     const project = req.body.project;
 
