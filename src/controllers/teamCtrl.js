fec227f9783182271a03de266a2e59325dadf14d
q-ode <bayodapo@gmail.com>
2017-03-06 17:22:31 +0100

    Formatting response

--- src/controllers/teamCtrl.js
+++ src/controllers/teamCtrl.js
@@ -11,7 +11,7 @@ const teamsCtrl = {
 
     teamFacade.setupNewTeamAndProject(team, project)
       .then((message) => {
-        res.send({ text: message.join(', ') });
+        res.send({ text: message.join('\n') });
       });
   }
 };
