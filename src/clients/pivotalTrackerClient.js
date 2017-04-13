4609e48a40d2802a9903458cfc9759e52059de17
q-ode <bayodapo@gmail.com>
2017-04-13 12:18:24 +0100

    Implement addUserToProject Implement createProjectAndAddUser

--- src/clients/pivotalTrackerClient.js
+++ src/clients/pivotalTrackerClient.js
@@ -1,19 +1,16 @@
 import Promise from 'bluebird';
-import log4js from 'log4js';
 import request from 'requestretry';
 
 const pivotalTrackerClient = {
-  baseUrl: process.env.PIVOTAL_TRACKER_BASE_API_URL,
-  token: process.env.PIVOTAL_TRACKER_TOKEN,
   accountId: process.env.PIVOTAL_TRACKER_ACCOUNT_ID,
 
   options: {
-    baseUrl: this.baseUrl,
+    baseUrl: process.env.PIVOTAL_TRACKER_BASE_API_URL,
     json: true,
     fullResponse: false,
     headers: {
       'Content-Type': 'application/json',
-      'X-TrackerToken': this.token
+      'X-TrackerToken': process.env.PIVOTAL_TRACKER_TOKEN
     }
   },
 
@@ -29,7 +26,7 @@ const pivotalTrackerClient = {
 
       request.post(options)
         .then(body => (body.kind !== 'error' ?
-          resolve('Pivotal Tracker: successful.')
+          resolve(body.id)
             : reject(`Pivotal Tracker: ${body.general_problem}`)
         ))
         .catch((error) => {
@@ -37,6 +34,44 @@ const pivotalTrackerClient = {
         });
     });
   },
+
+  addUserToProject(projectId, userEmail = 'adebayo.adesanya@andela.com') {
+    return new Promise((resolve, reject) => {
+      const options = this.options;
+
+      options.uri = `/projects/${projectId}/memberships`;
+      options.formData = {
+        email: userEmail,
+        role: 'owner'
+      };
+
+      request.post(options)
+        .then(body => (body.kind !== 'error' ?
+          resolve('Pivotal Tracker: user added successfully.') :
+          reject(`Pivotal Tracker: ${body.general_problem}`)))
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  },
+
+  createProjectAndAddUser(projectName) {
+    return new Promise((resolve, reject) => {
+      this.createProject(projectName)
+        .then((projectId) => {
+          this.addUserToProject(projectId)
+            .then(() => {
+              resolve('Pivotal Tracker: successful');
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
 
 };
 
