4d7bef21914d6ff5553488010011df873c056f2c
q-ode <bayodapo@gmail.com>
2017-04-20 12:17:56 +0100

    Remove RollBar integration

--- src/controllers/teamCtrl.js
+++ src/controllers/teamCtrl.js
@@ -1,12 +1,7 @@
-import rollbar from 'rollbar';
-
 import { teamFacade } from '../facades/teamFacade';
-
-rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN);
 
 const teamsCtrl = {
   create(req, res) {
-    rollbar.reportMessage(`Request: ${req.body.text}`);
     const payload = req.body.text;
     const setupInfo = payload.split(' ');
 
